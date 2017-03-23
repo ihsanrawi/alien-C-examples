0bd42b2c680f8b0018aefe0368cdd92770cf2db4
Flavio <flavioaugustosilveira@gmail.com>
2017-03-22 23:28:01 -0300

    Using Construct

---
+++ ex2/alien.hpp
@@ -0,0 +1,35 @@
+//
+//  main.cpp
+//  Alien
+//
+//  Copyright © 2017 Flavio. All rights reserved.
+//
+
+#ifndef alien_hpp
+#define alien_hpp
+
+#include <stdio.h>
+
+struct Alien
+{
+
+    int speed;
+    int year_time;
+    int light_years;
+    
+    // Adding a contruct to the Struct
+    // In C++, they do not use this->
+    // A good pratic is to put a underscore under the local variables
+    Alien(int _speed, int _year_time)
+    {
+    	speed = _speed;
+    	year_time = _year_time;
+    }
+
+    void CalcLightYears()
+    {
+        light_years = speed * year_time;
+    }
+};
+
+#endif /* alien_hpp */
---
+++ ex2/main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  Alien
+//
+//  Copyright © 2017 Flavio. All rights reserved.
+//
+
+#include <iostream>
+#include "alien.hpp"
+
+int main(int argc, const char * argv[]) {
+    
+    // Using the construct instead of set the elements one by one
+    Alien et1(100, 40);
+    Alien et2(130, 23);
+    
+    et1.CalcLightYears();
+    et2.CalcLightYears();
+    
+    printf("Light Years of ET1 %d\n", et1.light_years);
+    printf("Light Years of ET2 %d\n", et2.light_years);
+}
